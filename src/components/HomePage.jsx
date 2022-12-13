66706aa8e519d712c6230fc6eae405b37d40b3cc
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-13 23:04:52 +0600

    counter added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import counterReducer, { decrement, increment } from '../features/counter/CounterSlice';
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    counter:counterReducer
+  }
+});
 const HomePage = () => {
+  const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
   // const counterReducer = () => {
   //   console.log('here');
   // }
@@ -10,9 +18,9 @@ const HomePage = () => {
   return (
     <>
       <div className='flex justify-center items-center '>
-        <h2></h2>
-        <button>Inc</button>
-        <button>Dec</button>
+        <h2 className='text-4xl font-bold'>{ count}</h2>
+        <button className='btn btn-primary' onClick={()=>dispatch(increment())}>Inc</button>
+        <button className='btn btn-primary' onClick={()=>dispatch(decrement())}>Dec</button>
         </div>
   </>
         );
