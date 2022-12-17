39a0a1d6e496fcd6956385f89d3f91835e9cfe50
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-17 23:11:00 +0600

    made workable hanburger menu

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Head.css'
+import { FaBars } from "react-icons/fa";
+import { useState } from 'react';
 
 const Head = () => {
+    const [open,setOpen]=useState(false)
     return (
-        <div className='flex flex-col lg:flex-row lg:justify-between mx-10'>
+        <div className="flex justify-between mx-10 lg:block">
+        <div className='flex flex-col lg:flex-row lg:justify-between '>
             {/* logo */}
             <div className="">
                 <h1>LOGO</h1>
             </div>
             {/* nav */}
-            <div className="">
-                <ul className='inline-flex flex-row space-x-4'>
-                    <li className='focus:btn-info focus:bg-green-500'>Home</li>
+            <div className={`${open?'block transition duration-300 ease-in-out':'hidden'} lg:block`}>
+                <ul className='inline-flex flex-col lg:flex-row space-x-4'>
+                    <li className='active:btn-info focus:btn-secondary'>Home</li>
                     <li className='active:btn-secondary focus:btn-info'>About</li>
                     <li className='active:btn-secondary focus:btn-info'>Contact</li>
                     <li className='active:btn-secondary focus:btn-info'>Blog</li>
@@ -21,7 +25,12 @@ const Head = () => {
             <div className="">
                 <button className='btn'>Get Started</button>
             </div>
-        </div>
+            {/* hamburger */}
+            </div>
+            <div className="lg:hidden">
+                <FaBars onClick={()=>setOpen(!open)}/>
+            </div>
+            </div>
     );
 };
 
