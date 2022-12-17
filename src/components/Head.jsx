8aee77171b62d2b38414c61616fb075f6d244896
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-17 21:50:39 +0600

    header customization

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,7 +3,7 @@ import './Head.css'
 
 const Head = () => {
     return (
-        <div className='flex lg:justify-between mx-10'>
+        <div className='flex flex-col lg:flex-row lg:justify-between mx-10'>
             {/* logo */}
             <div className="">
                 <h1>LOGO</h1>
@@ -11,15 +11,15 @@ const Head = () => {
             {/* nav */}
             <div className="">
                 <ul className='inline-flex flex-row space-x-4'>
-                    <li className='btn btn-sm focus:btn-info focus:bg-green-500'>Home</li>
-                    <li className='btn btn-sm active:btn-secondary focus:btn-info'>About</li>
-                    <li className='btn btn-sm active:btn-secondary focus:btn-info'>Contact</li>
-                    <li className='btn btn-sm active:btn-secondary focus:btn-info'>Blog</li>
+                    <li className='focus:btn-info focus:bg-green-500'>Home</li>
+                    <li className='active:btn-secondary focus:btn-info'>About</li>
+                    <li className='active:btn-secondary focus:btn-info'>Contact</li>
+                    <li className='active:btn-secondary focus:btn-info'>Blog</li>
                 </ul>
             </div>
             {/* button */}
             <div className="">
-                <button className='btn focus:text-6xl focus:bg-red-500'>Get Started</button>
+                <button className='btn'>Get Started</button>
             </div>
         </div>
     );
